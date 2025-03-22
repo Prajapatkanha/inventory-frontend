62356e8b214a6d4ae1bac8299a756438c5583238
Prajapatkanha <prajapatkanhaiya300@gmail.com>
2025-03-22 23:07:50 +0530

    Fixed backend URL issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://inventory-backend-rtc.onrender.com' // Render pe deployed backend
+    apiUrl: 'https://inventory-backend-rctc.onrender.com' // Render pe deployed backend
   };
-  
